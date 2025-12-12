64e3330a1c1e6b690bacc02abdb14e61b3ba0850
tr1x_em <admin@trix.is-a.dev>
2025-12-12 23:47:35 +0530

    workflow upd

--- build_and_commit.sh
+++ build_and_commit.sh
@@ -4,11 +4,12 @@ TIMESTAMP=$(date '+%Y-%m-%d %I:%M:%S %p')
 zola build --output-dir publish_tmp --force
 rsync -av --delete --exclude='.git' --exclude='CNAME' publish_tmp/ public/
 cd public
+git pull --rebase
 git add .
 git commit -m "Automated Deploy: $TIMESTAMP"
 git push
 
 cd ..
-git add .
-git commit -m "Automated Commit: $TIMESTAMP"
-git push
+# git add .
+# git commit -m "Automated Commit: $TIMESTAMP"
+# git push
--- content/_index.md
+++ content/_index.md
@@ -117,7 +117,7 @@ Back in ’06 I was still buffering… cosmic Wi-Fi was terrible
 
 <article class="anime fancy-list">
 
-**My All time favorites:**
+**All time favorites:**
 
 {{ anime_top() }}
 
@@ -174,6 +174,6 @@ Feel free to reach me out on any of these! Although I'm usually active on Discor
 
 </ul>
 
-# Projects
+## Projects
 
 #### Comming soon™
