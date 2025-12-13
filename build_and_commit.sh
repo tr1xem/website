5d53caa5481e63019ba750f6cf7b402c1989bca1
tr1x_em <admin@trix.is-a.dev>
2025-12-13 13:53:31 +0530

    Automated Commit: 2025-12-13 01:53:27 PM

--- build_and_commit.sh
+++ build_and_commit.sh
@@ -2,9 +2,11 @@
 
 TIMESTAMP=$(date '+%Y-%m-%d %I:%M:%S %p')
 zola build --output-dir publish_tmp --force
-rsync -av --delete --exclude='.git' --exclude='CNAME' publish_tmp/ public/
 cd public
 git pull --rebase
+cd ..
+rsync -av --delete --exclude='.git' --exclude='CNAME' publish_tmp/ public/
+cd public
 git add .
 git commit -m "Automated Deploy: $TIMESTAMP"
 git push
